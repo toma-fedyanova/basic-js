4ce30926523e060de75b82013ad70da6a7001cc4
Tamara Fedyanova <112993598+toma-fedyanova@users.noreply.github.com>
2023-04-16 19:53:16 +1000

    feat: add code

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -28,24 +28,53 @@ function repeater(str, options ) {
     res += options.addition;
     return res;
   }
+  else if ((str == options.addition) && (String(str) != 'null')) {
+    for (let i = 0; i < options.repeatTimes; i++) {
+      res +=str;
+      res += str;
+      res += '+';
+    } 
+    return res.slice(0, res.length - 1);
+  }
   else {
     for (let i = 0; i < options.repeatTimes; i++) {
      res += str;
      for (let j = 0; j < options.additionRepeatTimes; j++) {
       res += options.addition;
       if (options.additionSeparator)  res += options.additionSeparator;
+      else if (!options.additionSeparator && !options.separator)  res += '|';
+      else if (!options.additionSeparator) res += '|';   ////
       }
         if (options.separator) {
         if (options.additionSeparator) {
           res = res.slice(0, res.length - options.additionSeparator.length);
         }
+        if (!options.additionSeparator && (Object.keys(options).length != 2)) {          ////
+          res = res.slice(0, res.length - 1);
+        }
           res += options.separator;
      }  
+     else if (!options.additionSeparator && !options.separator) {  //
+      res = res.slice(0, res.length - 1);
+      res += '+';
+     }
+     else if (!options.separator && options.additionSeparator) {  ///
+    
+      res = res.slice(0, res.length - options.additionSeparator.length);
+      res += '+';
+    }
     }
     if (options.separator) {
       let num = options.separator.length;
       res = res.slice(0, res.length - num);
       }
+      else if (!options.additionSeparator && !options.separator) {  //
+        res = res.slice(0, res.length - 1);
+        
+       }
+       else if (!options.separator && options.additionSeparator) {  ///
+        res = res.slice(0, res.length - 1);
+        }
     return res;
   }
   }
